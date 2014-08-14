e449c76e817a2396aa6b07d0338dc3c064c6a353
Dragon Dave <dragon@scraperwiki.com>
2014-08-14 14:57:09 +0000

    email

---
+++ fail_email.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import smtplib
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+# me == my email address
+# you == recipient's email address
+
+def send_email(text, html):
+    me = "ds-bot@scraperwiki.com"
+    you = "dataservices@scraperwiki.com"
+
+    # Create message container - the correct MIME type is multipart/alternative.
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = "Broken scrapers"
+    msg['From'] = me
+    msg['To'] = you
+
+    # Record the MIME types of both parts - text/plain and text/html.
+    part1 = MIMEText(text, 'plain')
+    part2 = MIMEText(html, 'html')
+
+    # Attach parts into message container.
+    # According to RFC 2046, the last part of a multipart message, in this case
+    # the HTML message, is best and preferred.
+    msg.attach(part1)
+    msg.attach(part2)
+
+    # Send the message via local SMTP server.
+    s = smtplib.SMTP('localhost')
+    # sendmail function takes 3 arguments: sender's address, recipient's address
+    # and message to send - here it is sent as one string.
+    s.sendmail(me, you, msg.as_string())
+    s.quit()
