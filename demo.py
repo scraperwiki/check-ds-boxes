84fe24e1bc25df9d380eb4d77200e24055f4dd94
Dragon Dave <dragon@scraperwiki.com>
2014-08-14 13:57:46 +0000

    working query

--- demo.py
+++ demo.py
@@ -1,6 +1,21 @@
-from os import environ
+import datetime
 from pymongo import Connection
+
 with open("/home/secret") as f:
     conn_url = f.read().strip()
 d = Connection(conn_url).get_default_database()
-print d.users.count()
+
+yesterday = datetime.datetime.now()-datetime.timedelta(hours=24)
+
+
+query = {"status.type": "error",
+         "boxServer": "ds-ec2.scraperwiki.com",
+         "status.message": {"$ne": "Permission needed from Twitter"},
+         "status.updated": {"$gt": yesterday}}
+
+response = list(d.datasets.find(query).limit(10))
+
+most_recent = max(response, key=lambda x: x['status']['updated'])['status']['updated']
+print most_recent
+for x in response:
+    print x['displayName'], x['status']['message'], x['box']
